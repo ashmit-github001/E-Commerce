a04266c30f6ded0bc6fe9701e7e176c0f6e8fc46
ashmit-github001 <ashmitagarwal1225@gmail.com>
2025-02-02 02:32:11 +0530

    security config basic2

---
+++ ecommerce/src/main/java/com/ashmitagarwal/ecommerce/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.ashmitagarwal.ecommerce;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain configureSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		
+		httpSecurity
+		.authorizeHttpRequests(customizer -> {
+			customizer
+			.requestMatchers("/login.html", "/register.html").permitAll()
+			.anyRequest().authenticated();
+		})
+		.formLogin(customizer -> {
+			customizer
+			.loginPage("/login.html")
+			.loginProcessingUrl("/login")
+			.defaultSuccessUrl("/catalog.html")
+			.permitAll();
+		})
+		.csrf(customizer -> customizer.disable());
+		
+		return httpSecurity.build();		
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		UserDetails userDetails1 = User
+				.withUsername("ram")
+				.passwordEncoder(password -> passwordEncoder().encode(password)) //OR .passwordEncoder(passwordEncoder()::encode)
+				.password("test")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(userDetails1);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+}
